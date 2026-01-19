2c6e975b151c29980a61ed597ce6d7ae27c51861
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-01-19 16:25:38 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js?v=2') // v=2 डालने से ब्राउज़र नई फाइल उठाएगा
-    .then(() => console.log("Service Worker Registered"))
-    .catch(err => console.log("SW Error:", err));
-}
