d4ab673e41cf9e39ce3dbc6346bb0161877e8ad3
moazzamazmi333-ui <moazzamazmi333@gmail.com>
2026-01-19 16:43:15 +0530

    Update sw.js

--- note/sw.js
+++ note/sw.js
@@ -1,5 +1,44 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js?v=2') // v=2 डालने से ब्राउज़र नई फाइल उठाएगा
-    .then(() => console.log("Service Worker Registered"))
-    .catch(err => console.log("SW Error:", err));
-}
+// Service Worker Name and Version
+const CACHE_NAME = 'bookgadi-note-v1';
+
+// Files to Cache (Offline support ke liye)
+const urlsToCache = [
+  '/note/',
+  '/note/index.html',
+  '/note/manifest.json'
+];
+
+// Install Event: Files ko cache mein save karta hai
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event: Purane cache ko delete karta hai
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event: Isse "Install Popup" trigger hota hai
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
